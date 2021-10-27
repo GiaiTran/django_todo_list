7a8671167674f8a73191da72606768a35352d579
GiaiTran <39039933+GiaiTran@users.noreply.github.com>
2021-10-27 12:51:03 -0500

    Add TaskCreate Class

--- base/views.py
+++ base/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
 from .models import Task
 
 # Create your views here.
@@ -17,3 +18,6 @@ class TaskDetail(DetailView):
     model = Task
     context_object_name = "task"
     template_name = 'base/task.html'
+
+class TaskCreate(CreateView):
+    model = Task
