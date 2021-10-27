ba5020c63cbc4efe6f1b0976524609dd1aeb6f62
GiaiTran <39039933+GiaiTran@users.noreply.github.com>
2021-10-26 23:07:49 -0500

    Added TaskList and TaskDetail

--- base/views.py
+++ base/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from .models import Task
 
 # Create your views here.
-def taskList(request):
-    return HttpResponse("Base")
+# def taskList(request):
+#     return HttpResponse("Base")
+
+class TaskList(ListView):
+    model = Task
+    context_object_name = "tasks"
+    # template_name = 'base/'
+
+class TaskDetail(DetailView):
+    model = Task
+    context_object_name = "task"
+    template_name = 'base/task.html'
